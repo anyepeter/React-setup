41562489c5345b751e64a56b4f8f5d3d5c126326
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-12-03 23:54:11 +0100

    loop through an array of animals

--- src/SearchParms.js
+++ src/SearchParms.js
@@ -1,15 +1,38 @@
 import { useState } from 'react'
 
+const ANIMAL = ["brids", "cats", "dogs", "rabbit", "reptiles"];
+
 const SearchParams = () => {
   // const location = "Seattle, WA";
-// const [location, setLocation] = useState("");
+const [location, setLocation] = useState("");
+const [animal, setAnimal] = useState("");
   return (
  <div className="search-params"> 
  <form>
      <label htmlFor="location">
       location
-        <input id="location" placeholder="location"  />
+        <input id="location" value={location} placeholder="location" onChange={(e) => setLocation(e.target.value)} />
      </label>
+     <label htmlFor="animal">
+      animal
+      <select id="animal"
+      value={animal}
+      onchange={ (e) => {
+        setAnimal(e.target.value);
+        setBreed("")
+      } }
+      onBlur={(e) =>{
+        setAnimal(e.target.value);
+        setBreed("")
+      }
+      } >
+        <option {...ANIMAL.map((animal) => {
+          return (
+            <option key={animal} value={animal}>{animal}</option>
+          )
+        })}> </option>
+      </select>
+      </label>
        <button>Submit</button>
   </form>
   </div>
